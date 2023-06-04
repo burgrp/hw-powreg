676f66b094fffdf00a8c2c3b72ed6ca0e44467e7
Pavel Burgr <pb@drake.cz>
2023-06-04 15:13:55 +0200

    blink

--- line/fw/src/main.zig
+++ line/fw/src/main.zig
@@ -5,11 +5,6 @@ const peripherals = micro.chip.peripherals;
 
 pub const microzig_options = struct {
     pub const interrupts = struct {
-
-        //peripherals.PORTA.OUTTGL = 1 << 3;
-        // peripherals.PORTA.OUTCLR = 1 << 3;
-        // peripherals.RTC.PITINTFLAGS.write(.{ .PI = 1, .padding = 0 });
-
         pub fn TCA0_LUNF() void {
             peripherals.PORTA.OUTTGL = 1 << 3;
             peripherals.TCA0.SINGLE.INTFLAGS.modify(.{ .OVF = 1 });
@@ -20,18 +15,14 @@ pub const microzig_options = struct {
 pub fn main() void {
     peripherals.PORTA.DIR = (1 << 3) | (1 << 2);
 
-    peripherals.CPU.CCP.write(.{ .CCP = .{ .value = .IOREG } });
-    peripherals.CPUINT.CTRLA.modify(.{ .CVT = 1, .IVSEL = 1 });
-
     peripherals.TCA0.SINGLE.CTRLA.modify(.{ .ENABLE = 1, .CLKSEL = .{ .value = .DIV16 } });
     peripherals.TCA0.SINGLE.INTCTRL.modify(.{ .OVF = 1 });
+
+    peripherals.SLPCTRL.CTRLA.modify(.{ .SMODE = .{ .value = .IDLE }, .SEN = 1 });
 
     micro.cpu.enable_interrupts();
 
     while (true) {
-        // if (peripherals.TCA0.SINGLE.INTFLAGS.read().OVF == 1) {
-        //     peripherals.TCA0.SINGLE.INTFLAGS.modify(.{ .OVF = 1 });
-        //     peripherals.PORTA.OUTTGL = 1 << 2;
-        // }
+        asm volatile ("sleep");
     }
 }
