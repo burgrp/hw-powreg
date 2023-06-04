26312283d4dd6ae2404f42d108608ccc84521576
Pavel Burgr <pb@drake.cz>
2023-06-05 00:52:43 +0200

    WIP

--- line/fw/src/main.zig
+++ line/fw/src/main.zig
@@ -3,26 +3,80 @@ const micro = @import("microzig");
 //const peripherals = @import("../deps/microzig-avr/src/chips/ATtiny412.zig").devices.ATtiny412.peripherals;
 const peripherals = micro.chip.peripherals;
 
+const GATE_PIN = 3;
+const ERR_PIN = 2;
+const ZCD_PIN = 1;
+
+var period: u16 = 0;
+
 pub const microzig_options = struct {
     pub const interrupts = struct {
         pub fn TCA0_LUNF() void {
-            peripherals.PORTA.OUTTGL = 1 << 3;
+            // gate off
+            peripherals.PORTA.OUTCLR = 1 << GATE_PIN;
+            // stop timer
+            peripherals.TCA0.SINGLE.CTRLA.modify(.{ .ENABLE = 0 });
+            // show error
+            peripherals.PORTA.OUTSET = 1 << ERR_PIN;
+            // clear interrupt flag
             peripherals.TCA0.SINGLE.INTFLAGS.modify(.{ .OVF = 1 });
+        }
+        pub fn TCA0_CMP0() void {
+            // gate on
+            peripherals.PORTA.OUTSET = 1 << GATE_PIN;
+            // clear interrupt flag
+            peripherals.TCA0.SINGLE.INTFLAGS.modify(.{ .CMP0 = 1 });
+        }
+        pub fn TCA0_CMP1() void {
+            // gate off
+            peripherals.PORTA.OUTCLR = 1 << GATE_PIN;
+            // clear interrupt flag
+            peripherals.TCA0.SINGLE.INTFLAGS.modify(.{ .CMP1 = 1 });
+        }
+        pub fn PORTA_PORT() void {
+
+            // if timer is running, measure period
+            if (peripherals.TCA0.SINGLE.CTRLA.read().ENABLE == 1) {
+                period = peripherals.TCA0.SINGLE.CNT;
+
+//peripherals.PORTA.OUTSET = 1 << GATE_PIN;
+                peripherals.TCA0.SINGLE.CMP0 = 1000;//period / 16;
+                peripherals.TCA0.SINGLE.CMP1 = period / 4;
+            }
+
+            // start timer from zero
+            peripherals.TCA0.SINGLE.CNT = 0;
+            peripherals.TCA0.SINGLE.CTRLA.modify(.{ .ENABLE = 1 });
+
+            // clear error
+            peripherals.PORTA.OUTCLR = 1 << ERR_PIN;
+
+            // clear interrupt flag
+            peripherals.PORTA.INTFLAGS.write(.{ .INT = 1 << ZCD_PIN });
         }
     };
 };
 
 pub fn main() void {
-    peripherals.PORTA.DIR = (1 << 3) | (1 << 2);
+    peripherals.PORTA.DIR = (1 << GATE_PIN) | (1 << ERR_PIN);
+    peripherals.PORTA.PIN1CTRL.modify(.{ .PULLUPEN = 0, .ISC = .{ .value = .BOTHEDGES } });
 
-    peripherals.TCA0.SINGLE.CTRLA.modify(.{ .ENABLE = 1, .CLKSEL = .{ .value = .DIV16 } });
-    peripherals.TCA0.SINGLE.INTCTRL.modify(.{ .OVF = 1 });
+    // CPU at 10MHz
+    peripherals.CPU.CCP.write_raw(0xD8);
+    peripherals.CLKCTRL.MCLKCTRLB.modify(.{ .PDIV = .{ .value = .@"2X" } });
 
-    peripherals.SLPCTRL.CTRLA.modify(.{ .SMODE = .{ .value = .IDLE }, .SEN = 1 });
+    // TCA0 at 2.5MHz, overflow at ~38Hz
+    peripherals.TCA0.SINGLE.CTRLA.modify(.{ .ENABLE = 0, .CLKSEL = .{ .value = .DIV4 } });
+    peripherals.TCA0.SINGLE.INTCTRL.modify(.{ .OVF = 1, .CMP0 = 1, .CMP1 = 1 });
 
     micro.cpu.enable_interrupts();
 
+    peripherals.SLPCTRL.CTRLA.modify(.{ .SMODE = .{ .value = .IDLE }, .SEN = 1 });
     while (true) {
         asm volatile ("sleep");
     }
 }
+
+// export fn foo(a: u8, b: u16) i32 {
+//     return a * b / 256;
+// }
